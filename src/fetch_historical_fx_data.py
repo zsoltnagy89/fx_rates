74c3a3c36f72de0fdc8f7a43c076bacb1f480c4a
Zsolt Nagy <zsolt.nagy@happysocks.com>
2025-12-02 19:38:40 +0100

    add historical fx fetcher

---
+++ src/fetch_historical_fx_data.py
@@ -0,0 +1,45 @@
+import os
+from dotenv import load_dotenv
+import time
+from datetime import date
+import requests
+import json
+from pathlib import Path
+
+# Load .env
+load_dotenv()
+access_key = os.getenv("API_KEY_FX")
+if not access_key:
+    raise ValueError("API_KEY_FX not found in .env")
+
+print("API KEY loaded:", access_key is not None)
+
+# Endpoint
+endpoint = 'timeframe'
+
+# JSON mappa létrehozása a projekt root alapján
+script_dir = Path(__file__).parent.resolve()
+json_dir = script_dir.parent / "jsons" / "fx_jsons"
+json_dir.mkdir(parents=True, exist_ok=True)
+
+# Évek 1999 → tavaly
+start_year = 1999
+current_year = date.today().year
+
+for year in range(start_year, current_year):
+    print(f"Year {year} is started!")
+    start_date = f"{year}-01-01"
+    end_date = f"{year}-12-31"
+
+    response = requests.get(
+        f'https://api.exchangerate.host/{endpoint}?access_key={access_key}&start_date={start_date}&end_date={end_date}'
+    )
+    data_tf = response.json()
+
+    output_file = json_dir / f"usd_other_fx_rates_{year}.json"
+    with open(output_file, "w", encoding="utf-8") as f:
+        json.dump(data_tf, f, indent=4, ensure_ascii=False)
+
+    print(f"Year {year} is finished!")
+    # Wait 0.5 sec to avoid rate limit
+    time.sleep(0.5)
